39c5e329325528ba8647ceee2e5c9ee824a79968
summ3rproj3ct <adamfitriyan666@gmail.com>
2022-04-16 08:12:29 +0700

    new update(2)

--- login.php
+++ login.php
@@ -75,7 +75,7 @@ if( isset($_POST["login"]) ) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Brian D</title>
+    <title>Login Page</title>
     <!-- <link rel="icon" type="image/x-icon" href="assets/img/favicon-32x32.png"> -->
     <link rel="icon" type="image/x-icon" href="assets/img/favicon-32x32.png">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
@@ -84,25 +84,22 @@ if( isset($_POST["login"]) ) {
 <body class="general">
        <!-- Header Section -->
     
-    <div class="container-fluid" id="container-fluid">
-        <div class="text-center">
-            <form action="" method="post" id="form-login" class="bg-white border rounded">
-                <div class="mt-2">
-                    <a href="https://albashiroh.com" class="btn btn-outline-dark">Ke Webpage Utama</a>
-                </div>
-                <h1 class="h3 mt-3 mb-3 font-weight-normal text-dark fs-1 font-monospace">Halaman Login</h1>
-    
-                <img src="img/logoRQS.png" alt="Logo Albashiroh" class="w-50 mb-5">
+    <div class="container-fluid login w-25 mt-5 p-0" id="container-fluid">
+        <div class="card text-center rounded">
+            <form action="" method="post" id="form-login" class="bg-white border rounded  rounded-3 p-2">
+                
+                <img src="assets/img/logo.png" alt="Brian D" class="mb-2 mt-3">
+                <h1 class="h3 mt-3 mb-5 font-weight-normal text-dark fs-1 font-monospace">Halaman Login</h1>
 
                 <?php if( isset($error) ) :?>
                     <div class="alert alert-warning text-danger">Username atau password salah</div>
                 <?php endif; ?>
 
                         <label for="username" class="visually-hidden">Username</label>
-                        <input type="text" name="username" id="username" class="form-control" placeholder="Username" autofocus autocomplete="off" required>
+                        <input type="text" name="username" id="username" class="form-control rounded-0 rounded-top" placeholder="Username" autofocus autocomplete="off" required>
 
                         <label for="password" class="visually-hidden">Password :</label>
-                        <input type="password" name="password" id="password" class="form-control rounded-bottom" placeholder="Password">
+                        <input type="password" name="password" id="password" class="form-control rounded-0 rounded-bottom" placeholder="Password">
 
                         <div class="checkbox mt-3">
                             <input type="checkbox" name="remember" id="remember">
@@ -110,10 +107,10 @@ if( isset($_POST["login"]) ) {
                         </div>
 
                         <div class="d-grid mt-3 col-3 m-auto">
-                        <button type="submit" name="login" class="btn btn-primary btn-lg">Login</button>
+                        <button type="submit" name="login" class="btn btn-sm btn-outline-primary">Login</button>
                         </div>
 
-                    <p class="mt-5 ">Untuk informasi lebih lanjut, hubungi kami </p>
+                    <!-- <p class="mt-5 ">Untuk informasi lebih lanjut, hubungi kami </p>
                     <a href="https://id-id.facebook.com/RQAlBashiroh" target="_blank" class="col btn btn-outline-primary mt-2">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-facebook" viewBox="0 0 16 16">
                         <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z"/>
@@ -128,9 +125,9 @@ if( isset($_POST["login"]) ) {
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-whatsapp" viewBox="0 0 16 16">
                         <path d="M13.601 2.326A7.854 7.854 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.933 7.933 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.898 7.898 0 0 0 13.6 2.326zM7.994 14.521a6.573 6.573 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.557 6.557 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592zm3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.729.729 0 0 0-.529.247c-.182.198-.691.677-.691 1.654 0 .977.71 1.916.81 2.049.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232z"/>
                         </svg> Whatsapp
-                    </a>
+                    </a> -->
 
-                    <p class="mt-3 ">&copy;Copyright by Adam Fitriyan<p>
+                    <p class="mt-3 ">&copy;Copyright by Brian Dratolla<p>
 
             </form>
             
